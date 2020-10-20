673beede009fba9648480b3f5dcec95c8be38d89
yuangan <411246903@qq.com>
2020-10-20 14:39:18 +0800

    added wrap

--- optical_flow_mesh.py
+++ optical_flow_mesh.py
@@ -97,6 +97,7 @@ def method_list_comprehension(from_values, arr, to_values1, to_values2):
 
 def optical_cal(tri1, tri2):
     outpath = f'examples/results/_flow_search.jpg'
+    outpath2 = f'examples/results/_flow_ref.jpg'
     resx = np.zeros_like(tri2)
     resy = np.zeros_like(tri2)
 
@@ -104,6 +105,7 @@ def optical_cal(tri1, tri2):
     tri2_view = tri2.ravel()
 
     val, tri1_diff, tri2_diff = np.intersect1d(tri1_view, tri2_view, assume_unique=False, return_indices=True)
+    ## val重叠面片序号集合
     val = val[1:]
     tri1_diff = tri1_diff[1:]
     tri2_diff = tri2_diff[1:]
@@ -112,15 +114,14 @@ def optical_cal(tri1, tri2):
     h = tri1.shape[1]
     # print(w, h)
 
-
-    y1 = tri1_diff%w
-    x1 = np.floor(tri1_diff/w).astype(int)
-    y2 = tri2_diff%w
-    x2 = np.floor(tri2_diff/w).astype(int)
+    x1 = tri1_diff%w
+    y1 = np.floor(tri1_diff/w).astype(int)
+    x2 = tri2_diff%w
+    y2 = np.floor(tri2_diff/w).astype(int)
     # hsv = np.zeros((h, w, 3), np.uint8)
     detax = x1 - x2
     detay = y1 - y2
-    
+
     # broadcast
     #for i in range(1, len(val)):
     #    resx[tri1 == val[i]] = detax[i]
@@ -131,13 +132,23 @@ def optical_cal(tri1, tri2):
     resx, resy = method_search2(val, tri1_view, detax, detay)
 
 
-    flow = np.asarray([resx, resy]).transpose(1,2,0)
-    print(flow.shape)
-    flow = viz_flow(flow.astype(float))
-    #res[x1, y1] = detax%255 + detay%255
-    #res2[x2, y2] = 255
-    cv2.imwrite(outpath, flow)
+    flow = np.asarray([resy, resx]).transpose(1,2,0)
+    # print(flow.shape)
+    color = viz_flow(flow.astype(float))
+    cv2.imwrite(outpath, color)
     #cv2.imwrite(outpath.replace('ti','ti2'), res2)
+
+    #ref
+    resx2, resy2 = method_search2(val, tri2_view, detax, detay)
+    #a = np.zeros_like(resx2)
+    #a -= 10
+    flow2 = np.asarray([resy2, resx2]).transpose(1,2,0)
+    color2 = viz_flow(flow2.astype(float))
+    cv2.imwrite(outpath2, color2)
+
+    return flow2
+    #return flow
+
 
 def viz_flow(flow):
     # 色调H：用角度度量，取值范围为0°～360°，从红色开始按逆时针方向计算，红色为0°，绿色为120°,蓝色为240°
@@ -145,7 +156,7 @@ def viz_flow(flow):
     # 亮度V：取值范围为0.0(黑色)～1.0(白色)
     h, w = flow.shape[:2]
     hsv = np.zeros((h, w, 3), np.uint8)
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    mag, ang = cv2.cartToPolar(flow[...,1], flow[...,0])
     hsv[...,0] = ang*180/np.pi/2
     hsv[...,1] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
     # flownet是将V赋值为255, 此函数遵循flownet，饱和度S代表像素位移的大小，亮度都为最大，便于观看
@@ -153,6 +164,19 @@ def viz_flow(flow):
     hsv[...,2] = 255
     bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
     return bgr
+
+def wrap(img_path, flow):
+    img = cv2.imread(img_path)
+    output = np.zeros_like(img)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if flow[i, j, 0] != 0 or flow[i, j, 1] != 0:
+                try:
+                    output[i + flow[i, j, 0], j + flow[i, j, 1]] = img[i, j]
+                except:
+                    print(i + flow[i, j, 0], j + flow[i, j, 1], flow[i,j])
+    cv2.imwrite('examples/results/_wrap.jpg', output)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='The demo of still image of 3DDFA_V2')
@@ -169,4 +193,6 @@ if __name__ == '__main__':
     img_tri = main(args)
     args.img_fp = args.img_ref
     img_ref_tri = main(args)
-    optical_cal(img_tri, img_ref_tri)
+    flow = optical_cal(img_tri, img_ref_tri)
+
+    wrap(args.img_ref, flow)
